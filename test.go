c64539022f3bc055e4e367b5d54fb51ea64150f1
Carolyn Saund <carolyn@amygdala>
2015-01-19 22:54:02 -0500

    placing stones!

--- test.go
+++ test.go
@@ -1,50 +1,75 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 )
 
 func main() {
 	b := newBoard(10)
-	b.printBoard()
-}
-
-func helloworld(){
-	//no python arrays, just "slices"
-	//declare slice using "make" command, like new (slice/map/struct/C things)
-	strs := make([]string, 0) //dynamic, make is like new
-	strs = append(strs, "Hello,")
-	strs = append(strs, "world!")
-	//both of these are slices	cause []
-	str := strings.Join(strs, " ")
-	fmt.Println(str)
+	var i,j int
+	var err error
+	for {
+		b.printBoard()
+		for{
+			i, j = getMove()
+			err = b.placeStone(Wpiece, i, j)
+			if err == nil{
+				break
+			}
+		}
+		b.printBoard()
+		for{
+			i, j = getMove()
+			err = b.placeStone(Bpiece, i, j)
+			if err == nil{
+				break
+			}
+		}
+	}
 }
 
 const (
-	Nopiece=iota
+	Nopiece = iota
 	Wpiece
 	Bpiece
 )
 
-type Board struct{ 
-	cells [][]int 
-	size int
+type Board struct {
+	cells [][]int
+	size  int
+}
+
+func getMove() (i, j int) {
+	//prompt user
+	//get things
+	for {
+		fmt.Println("please enter i j")
+		var i, j int
+		_, err := fmt.Scanf("%d %d\n", &i, &j)
+		if err == nil {
+			return i, j
+		} else {
+			fmt.Println("Invalid move. Try again")
+		}
+	}
+
 }
 
 func (b *Board) init() {
-	for _,i := range(b.cells){
-		for ind,_ := range(i){
+	for _, i := range b.cells {
+		for ind, _ := range i {
 			i[ind] = Nopiece
 		}
 	}
 }
 
-func newBoard(sqsize int)(*Board){
+func newBoard(sqsize int) *Board {
 	//new different from make. Make only on primitives
 	b := new(Board)
 	b.cells = make([][]int, sqsize)
-	for i := range(b.cells){
+	for i := range b.cells {
 		b.cells[i] = make([]int, sqsize)
 	}
 	b.init()
@@ -52,22 +77,35 @@ func newBoard(sqsize int)(*Board){
 	return b
 }
 
-func (b *Board) printBoard(){
-	sep := strings.Repeat("-", b.size*4 + 1)
+func (b *Board) printBoard() {
+	sep := strings.Repeat("-", b.size*4+1)
 	fmt.Println(sep)
-	for _,i := range(b.cells){
+	for _, i := range b.cells {
 		lnstr := make([]string, b.size)
-		for ind,_ := range(i){
-			if i[ind] == Wpiece{
+		for ind, _ := range i {
+			if i[ind] == Wpiece {
 				lnstr[ind] = " O "
-			} else if i[ind] == Bpiece{
+			} else if i[ind] == Bpiece {
 				lnstr[ind] = " X "
-			} else{
-				lnstr[ind] = "   "		
+			} else {
+				lnstr[ind] = "   "
 			}
 		}
 		line := strings.Join(lnstr, "|")
 		fmt.Printf("|%s|\n", line)
 		fmt.Println(sep)
 	}
+}
+
+func (b *Board) placeStone(Player, i, j int) error {
+	if b.cells[i][j] != Nopiece {
+		return errors.New("Board cell already occupied")
+	} else {
+		b.cells[i][j] = Player
+	}
+	return nil
+}
+
+func (b *Board) effectBoard() {
+
 }
